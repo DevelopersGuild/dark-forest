a87cea84446af7182aed03ae6b30639695944192
deeptir24 <deeptir24@gmail.com>
2017-02-03 15:36:06 -0800

    New Changes to Player Class
    Complete code for movement of the player class, based on position of
    walls.

--- dark-forest-master/Player.h
+++ dark-forest-master/Player.h
@@ -6,35 +6,40 @@ class MainPlayer : Walls
 {
 private:
     int cposition[4][2]; // current posistion
-    
+    int COLS = 31;
     
 public:
     // Make a constructor that sets the origin of the player at {-300,0}
     // Make a function that updates the position after moving a specific direction
     
-    void MoveRight(int cposition[][COLS], int ROWS)
+    void MoveRight(int ROWS, int cposition[4][2], bool ifwall)
     {
-        ROWS++; // If moving right, need to increase the x-axis position.
+        if (ifwall == false)
+            ROWS++; // If moving right, need to increase the x-axis position.
         
         
     }
-    void MoveLeft(int cposition[][COLS], int ROWS)
+    void MoveLeft(int cposition[4][2], int ROWS, bool ifwall)
     {
-        ROWS--; // If moving right, need to increase the x-axis position.
+        if (ifwall == false)
+            
+            ROWS--; // If moving right, need to increase the x-axis position.
         
         
     }
-    void MoveUp(int cposition[][COLS], int ROWS)
+    void MoveUp(int cposition[4][2], int ROWS, bool ifwall)
     {
-        COLS--; // If moving right, need to increase the x-axis position.
+        if (ifwall == false)
+            
+            COLS--; // If moving right, need to increase the x-axis position.
         
         
     }
-    void MoveDown(int cposition[][COLS], int ROWS)
+    void MoveDown(int cposition[4][2], int ROWS, bool ifwall)
     {
-        COLS--; // If moving right, need to increase the x-axis position. 
-        
-        
+        if (ifwall == false)
+            
+            COLS--; // If moving right, need to increase the x-axis position. 
     }
     
 };
--- dark-forest-master/main.cpp
+++ dark-forest-master/main.cpp
@@ -1,19 +1,17 @@
-#include <iostream>
-
 #include <SFML/Graphics.hpp>
 #include "ResourcePath.h"
 
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(300, 300), "SFML Works!");
-    
-    // white piece of wall at position 20, 30
+    window.setVerticalSyncEnabled(true);
+    // create a wall in the shape of rectangle
     sf::RectangleShape rect;
     rect.setSize(sf::Vector2f(20.f, 10.f));
     rect.setFillColor(sf::Color::White);
     rect.setPosition(20.0, 30.0);
     
-    // circle shape
+    // create the main player in the shape of a circle for now
     sf::CircleShape player(20.f);
     player.setFillColor(sf::Color::Green);
     player.setOrigin(-150.f, -150.f);
@@ -27,28 +25,28 @@ int main()
                 window.close();
         }
         
-        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
         {
             // Update the position, and check whether that value is equal to the bool value returned by the walls coordinates. If it's true, call the function that updates the position
             
-            player.move(0, -0.25);
+            player.move(0, -2.00);
         }
         
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
         {
-            player.move(0.0, 0.25);
+            player.move(0.0, 2.00);
             
         }
         
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
         {
-            player.move(-0.25, 0.0);
+            player.move(-2.00, 0.0);
         }
         
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
         {
             
-            player.move(0.25, 0.0);
+            player.move(2.00, 0.0);
         }
         
         
