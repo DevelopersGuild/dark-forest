62619e1df969db61f8a3396a359895602522df30
MYE1102 <MYE1102>
2017-02-03 15:07:36 -0800

    Changed IntroDialogue
    Rathna & Ye Eun

--- sfml-game-template-master/src/IntroDialogue.cpp
+++ sfml-game-template-master/src/IntroDialogue.cpp
@@ -1,2 +1,20 @@
 #include "IntroDialogue.h"
 
+IntroDialogue::IntroDialogue()
+{
+     txt.open(resourcePath() + "assets/startDia.txt");
+
+}
+void IntroDialogue::openfolder(std::ifstream &txt)
+{
+     if (!txt.is_open())
+     {
+          std::cout << "Cannot open the startDia.txt!!\n";
+     }
+
+     while (txt.eof())
+     {
+          
+     }
+
+}
