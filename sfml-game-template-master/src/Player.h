10e65b9bf5cf7003930be782d064651093a1dd51
deeptir24 <deeptir24@gmail.com>
2017-02-10 14:45:13 -0800

    New Player Movement w/ Boundaries

--- sfml-game-template-master/src/Player.h
+++ sfml-game-template-master/src/Player.h
@@ -22,16 +22,23 @@ public:
     // Make a function that updates the position after moving a specific direction
     void MovePlayer(int ROWS, int cposition[4][2], bool ifwall, string keypressed)
     {
-        if (ifwall == false
+        if (ifwall == false)
         {
-            if (kepressed == "Right")
-                ROWS++; // If moving right, need to increase the x-axis position.
-            if (keypressed == "Left")
-                ROWS--;
-            if (keypressed == "Up")
-                COLS++;
-            if (keypressed == "Down")
-                COLS--;
+            if (sf::Keyboard::isKeyPressed(sf::Key::Right))
+                if (COLS >=0 && COLS <300)
+                    COLS++; // If moving right, need to increase the x-axis position.
+            
+            if (sf::Keyboard::isKeyPressed(sf::Key::Left))
+                if (COLS >0 && COLS <=300)
+                    COLS--; // If the player is moving left, descrease the x axis position.
+            
+            if (sf::Keyboard::isKeyPressed(sf::Key::Up))
+                if (ROWS >0 && ROWS <=300 )
+                ROWS++; // If the player is moving up, increase the y axis position.
+            
+            if (sf::Keyboard::isKeyPressed(sf::Key::Down))
+                if (ROWS >= 0 && ROWS <300) // Make sure that the position before the change is a valid area
+                    ROWS--; // If the player is moving down, decrease the y axis position.
                 
             }
         
