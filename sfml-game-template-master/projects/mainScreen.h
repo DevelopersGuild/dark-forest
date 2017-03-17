d630bbbe94322708081307e33ceffaca05a6fcea
shreyask123 <shreyask123@gmail.com>
2017-03-17 11:07:56 -0700

    merge

---
+++ sfml-game-template-master/projects/mainScreen.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <SFML/Graphics.hpp>
+#include "ResourcePath.h"
+class MainScreen
+{
+private:
+	sf::Font defaultFont;
+	void fontConfig();
+	sf::Text introText;
+	sf::Text continueDecisionText;
+	sf::Text closeDecisionText;
+	void textConfig(sf::Text *, int, int, std::string);
+	sf::RenderWindow window;
+	bool decision;
+public:
+	MainScreen::MainScreen();
+	bool getDecision();
+
+	~MainScreen();
+};
+
